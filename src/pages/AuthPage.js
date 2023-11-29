531b85e8349476ed450aa361f57cd1c65cff2694
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-11-29 14:25:02 +0200

    center login container

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,54 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import './AuthPage.css';
 
-const AuthPage = ({ history }) => {
-    const [username, setUsername] = useState(null);
-    const [userImg, setUserImg] = useState(null);
-    const navigate = useNavigate();
+const AuthPage = () => {
+  const [username, setUsername] = useState(null);
+  const [userImg, setUserImg] = useState(null);
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        // Додайте код для авторизації через Google або GitHub тут
-        // Після успішної авторизації використайте history.push('/empty-page') для переходу на порожню сторінку
-    };
+  const handleLogin = () => {
+    // Додайте код для авторизації через Google або GitHub тут
+    // Після успішної авторизації використайте history.push('/empty-page') для переходу на порожню сторінку
+  };
 
- 
+  useEffect(() => {
+    localStorage.setItem('userName', username);
+    localStorage.setItem('userImg', userImg);
+  }, [username, userImg]);
 
-    useEffect(() => {
-                localStorage.setItem('userName', username);
-                localStorage.setItem('userImg', userImg);
-    }, [username, userImg]);
-
-    console.log(username);
-    console.log(userImg);
-
-    return (
-        <div>
-            <h1>Авторизація</h1>
+  return (
+    <div className="auth-container">
+      <div className="centered-content">
+        <h1>Авторизація</h1>
+        <div className='google'>
             <GoogleLogin
-                onSuccess={credentialResponse => {
-                    console.log(credentialResponse);
-                    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-                    setUsername(credentialResponseDecoded.given_name);
-                    setUserImg(credentialResponseDecoded.picture);
-                    if(localStorage.getItem("userName") !== null) {
-                        console.log('умова пройшла')
-                        setTimeout(() => {
-                            navigate("/main-page");
-                            
-                            }, 2000)
-                        }}
-                        
-                    }
-              
-                onError={() => {
-                    console.log('Login Failed');
-                }}
+             style={{ backgroundColor: '#fff', color: '#000', border: '1px solid #000' }}
+            onSuccess={(credentialResponse) => {
+                const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+                setUsername(credentialResponseDecoded.given_name);
+                setUserImg(credentialResponseDecoded.picture);
+                if (localStorage.getItem('userName') !== null) {
+                setTimeout(() => {
+                    navigate('/main-page');
+                }, 2000);
+                }
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
             />
-            <button onClick={handleLogin}>Увійти через Google або GitHub</button>
         </div>
-    );
+     
+        <button className="login-button" onClick={handleLogin}>
+          Увійти через Google або GitHub
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
